f7bb28adc6a5f73772c164d68680fd780375648d
EcoChan <chengle@zealer.com>
2018-06-14 02:02:40 +0800

    分类页面样式

--- pages/allPhones/allPhones.js
+++ pages/allPhones/allPhones.js
@@ -1,17 +1,13 @@
 //logs.js
-const util = require('../../utils/util.js')
-
 Page({
-  data: {
-    companyes: [
-        {'name': ''},
-    ]
-  },
-  onLoad: function () {
-    this.setData({
-      logs: (wx.getStorageSync('logs') || []).map(log => {
-        return util.formatTime(new Date(log))
-      })
-    })
-  }
+    data: {
+        companyes: ['Apple', '三星', '华为', '小米', '一加','vivo','OPPO','锤子','诺基亚','魅族','努比亚','SONY','HTC','Google','LG','金立','TCL','IUNI','Microsoft']
+    },
+    onLoad: function () {
+        this.setData({
+            logs: (wx.getStorageSync('logs') || []).map(log => {
+                return util.formatTime(new Date(log))
+            })
+        })
+    }
 })
